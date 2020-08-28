d4afedfbc83bca3705b07d9921ce52319960c8a5
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-08-28 13:22:58 +0900

    0828 splash activity 완성 login activity xml 버튼은 전부? 있음 일단 자바 짬 근데 짜다보니까 회원 가입도 만들어야 돼서 만드는중 register activity 디자인 빼고 xml 완성
    오늘 첫 커밋★

--- app/src/main/java/com/example/edcan3vacationproject/RegisterActivity.java
+++ app/src/main/java/com/example/edcan3vacationproject/RegisterActivity.java
@@ -1,14 +1,29 @@
 package com.example.edcan3vacationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.edcan3vacationproject.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+        binding.setId("");
+        binding.setName("");
+        binding.setEmail("");
+        binding.setPw("");
+        binding.setPwcheck("");
 
     }
 }
