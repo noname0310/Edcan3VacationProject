864e9e090cdf622917da8958483c44d9db359c74
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-08-31 03:24:38 +0900

    0830      splash activity 완성      login activity 비밀번호하고 자동 로그인 남았다 시댕      register activity 완성
         누가 메인 activity 좀 해주세요 야발

--- app/src/main/java/com/example/edcan3vacationproject/MainActivity.java
+++ app/src/main/java/com/example/edcan3vacationproject/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.edcan3vacationproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.change_name:
+                break;
+            case R.id.logout:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
