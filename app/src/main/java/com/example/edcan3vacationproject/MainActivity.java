ca9c8f8c9afd9c943db19493ae2dc0a6867a2d0c
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-09-02 03:23:15 +0900

    Revert "0830"
    This reverts commit 4a8421a964ba0d1dbd14fbf98b233ca82aeabe27.

--- app/src/main/java/com/example/edcan3vacationproject/MainActivity.java
+++ app/src/main/java/com/example/edcan3vacationproject/MainActivity.java
@@ -8,36 +8,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.example.edcan3vacationproject.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StringReader;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
+import okio.Utf8;
+
 
 public class MainActivity extends AppCompatActivity {
-
     private Context mContext;
     private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
@@ -55,39 +50,30 @@ public class MainActivity extends AppCompatActivity {
     private int port = 20310;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        runOnUiThread(() -> {
-            super.onCreate(savedInstanceState);
-
-            int SDK_INT = android.os.Build.VERSION.SDK_INT;
-            if (SDK_INT > 8) {
-
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }
-
-            Toast.makeText(getApplicationContext(), "Connecting to server...", Toast.LENGTH_SHORT).show();
-            AsyncConnect("{}", (string) -> {
-                Toast.makeText(getApplicationContext(), string, Toast.LENGTH_LONG).show();
-                Log.i("TAG", string);
-            });
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler();
 
 
-            binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-//           binding.imgSendBtn.setOnClickListener(view -> {
-//               if (binding.editText3.getText().toString() != null || !binding.editText3.getText().toString().equals(""))
-//
-//           });
-            setSupportActionBar(binding.toolbar);
-            getSupportActionBar().setTitle("");
-            runOnUiThread(() -> {
-                Message message = new Message();
-            });
+//        try {
+//            setSocket(ip, port);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 
-        });
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        binding.imgSendBtn.setOnClickListener(view -> {
+//            if (binding.editText3.getText().toString() != null || !binding.editText3.getText().toString().equals(""))
+//                send();
+//        });
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle("");
+        Message message = new Message();
+        //ChatClient chatClient =  new ChatClient(UserCache.getUser(m));
 
     }
 
@@ -116,146 +102,39 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 //
-
-    @Override
-    protected void onStop() {  //앱 종료시
-        super.onStop();
-        try {
-            socket.close(); //소켓을 닫는다.
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void AsyncConnect(String initData, RecivedDataFunc recivedDataFunc) {
-        ConnectThread thread = new ConnectThread(initData, recivedDataFunc);
-        thread.start();
-    }
-
-    public void AsyncListening(RecivedDataFunc recivedDataFunc) {
-        ListenerThread thread = new ListenerThread(recivedDataFunc);
-        thread.start();
-    }
-
-    public void AsyncSend(String data) {
-        DataThread thread = new DataThread(data);
-        thread.start();
-    }
-
-    interface RecivedDataFunc {
-        void OnRecivedData(String data);
-    }
-
-    class ConnectThread extends Thread {
-        RecivedDataFunc recivedDataFunc;
-        String connectPacket;
-
-        public ConnectThread(String connectPacket, RecivedDataFunc recivedDataFunc) {
-            this.connectPacket = connectPacket;
-            this.recivedDataFunc = recivedDataFunc;
-        }
-
-        public void run() {
-            try {
-                socket = new Socket("112.154.88.112", 20310);
-
-                AsyncListening(recivedDataFunc);
-                AsyncSend(connectPacket);
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    class ListenerThread extends Thread {
-        RecivedDataFunc Func;
-        byte[] data;
-
-        public ListenerThread(RecivedDataFunc recivedDataFunc) {
-            this.Func = recivedDataFunc;
-        }
-
-        public String byteArrayToHex(byte[] a) {
-            StringBuilder sb = new StringBuilder();
-            for (final byte b : a)
-                sb.append(String.format("%02x ", b & 0xff));
-            return sb.toString();
-        }
-
-        public void run() {
-            try {
-                while (true) {
-                    InputStream input = socket.getInputStream();
-
-                    byte[] header = new byte[4];
-                    int recivedBytes = input.read(header);
-                    while (recivedBytes < 4) {
-                        input.read(header);
-                    }
-                    ByteBuffer wrapped = ByteBuffer.wrap(header);
-                    int contentsize = wrapped.getInt();
-
-                    data = new byte[contentsize];
-                    int recivedContentBytes = 0;
-                    while (recivedContentBytes < contentsize) {
-                        recivedContentBytes += input.read(data);
-                    }
-                    runOnUiThread(() -> Func.OnRecivedData(new String(data, StandardCharsets.UTF_8)));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    class DataThread extends Thread {
-        private String data;
-
-        public DataThread(String data) {
-            this.data = data;
-        }
-
-        public void run() {
-            try {
-                byte[] contentBuffer = data.getBytes(StandardCharsets.UTF_8);
-                byte[] header = ByteBuffer.allocate(4).putInt(contentBuffer.length).array();
-                byte[] sendBuffer = new byte[header.length + contentBuffer.length];
-                System.arraycopy(header, 0, sendBuffer, 0, header.length);
-                System.arraycopy(contentBuffer, 0, sendBuffer, header.length, contentBuffer.length);
-                OutputStream output = socket.getOutputStream();
-                output.write(sendBuffer);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void StringtoPacketType(String string) {
-        Gson gson = new Gson();
-        Packet convertedObject = (Packet)new Gson().fromJson(string, Packet.class);
-        switch (convertedObject.PacketType){
-            case Message:
-                Message recieveMsg = (Message) new Gson().fromJson(string, Message.class);
-                break;
-        }
-
-
-
-    }
-
-
-
-    public static String MsgToJson(Context context, Message msg) {
-
-        Gson msgGson = new Gson();
-        return msgGson.toJson(msg);
-    }
+//    public void setSocket(String ip, int port) throws IOException {
+//
+//        try {
+//            socket = new Socket(ip, port);
+//            networkWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+//            networkReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        } catch (IOException e) {
+//            System.out.println(e);
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public void send() {
+//        Message msg = new Message(new ChatClient(
+//                UserCache.getUser(mContext).getId(),
+//                UserCache.getUser(mContext).getName(),
+//                UserCache.getUser(mContext).getEmail()),
+//                binding.getMessage1().toString());
+//        String msgGson = MsgToJson(mContext, msg);
+//        byte[]  arr = msgGson.getBytes(StandardCharsets.UTF_8);
+//        byte[] header = ByteBuffer.allocate(4).putInt(arr.length).array();
+//        byte[] content = new byte[header.length + arr.length];
+//        System.arraycopy(header,0, content, 0, header.length );
+//        System.arraycopy(arr,0, content, header.length, arr.length );
+//
+//
+//
+//    }
+//
+//    public static String MsgToJson(Context context, Message msg) {
+//        Gson msgGson = new Gson();
+//        return msgGson.toJson(msg);
+//    }
 
 }
