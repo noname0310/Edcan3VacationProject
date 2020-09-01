91c36fcbb4ee9f29f2f9eb55dfcc1a79dfd75a03
noname <48761044+noname0310@users.noreply.github.com>
2020-09-02 05:27:36 +0900

    1

---
+++ GUIConsole/Assets/Scripts/Command/CommandObj.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GUIConsole.Command;
+using Assets.Scripts;
+
+public class CommandObj : MonoBehaviour
+{
+    public static CommandObj Instance { get; set; }
+
+    public GameObject inputFieldObj;
+    private InputField CommandInputField;
+
+    public delegate void CommandDelegate(string command, string[] args);
+    public Dictionary<string, CommandDelegate> CommandDelegates;
+    public CommandManager commandManager;
+    public SocketCommands SocketCommands;
+
+    private LinkedList<string> CommandBuffer;
+    private LinkedListNode<string> CommandBufferSearchIndex;
+
+    void Start()
+    {
+        Instance = this;
+
+        inputFieldObj = GameObject.Find("CommandInputField");
+        CommandInputField = inputFieldObj.GetComponent<InputField>();
+
+        CommandInputField.Select();
+        CommandInputField.ActivateInputField();
+
+        CommandDelegates = new Dictionary<string, CommandDelegate>();
+        commandManager = new CommandManager();
+        SocketCommands = new SocketCommands(ServerObj.chatServer);
+
+        CommandBuffer = new LinkedList<string>();
+        CommandBufferSearchIndex = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            string cmd = CommandInputField.text;
+
+            if (cmd.Trim() != "")
+            {
+                if (CommandBuffer.Count == 0 || CommandBuffer.Last.Value != cmd.Trim())
+                {
+                    CommandBuffer.AddLast(cmd);
+
+                    if (CommandBuffer.Count > 50)
+                    {
+                        CommandBuffer.RemoveFirst();
+                    }
+                }
+            }
+
+            IGConsole.Instance.printWarnln(">" + cmd);
+            var ParsedCmd = CommandArgsParser.ParseArgs(cmd);
+
+            CommandDelegate Command;
+            if(CommandDelegates.TryGetValue(ParsedCmd.Item1.ToUpper(), out Command))
+            {
+                Command(cmd, ParsedCmd.Item2);
+            }
+            else
+            {
+                IGConsole.Instance.println("\"" + ParsedCmd.Item1 + "\" is not exist command");
+            }
+
+            CommandInputField.text = "";
+            CommandInputField.Select();
+            CommandInputField.ActivateInputField();
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (CommandBuffer.Count == 0)
+                return;
+
+            if (CommandBufferSearchIndex == null)
+            {
+                CommandBufferSearchIndex = CommandBuffer.Last;
+            }
+            else if (CommandInputField.text.Trim() == "")
+            {
+            }
+            else if (CommandBufferSearchIndex.Previous != null)
+            {
+                CommandBufferSearchIndex = CommandBufferSearchIndex.Previous;
+            }
+
+            CommandInputField.text = CommandBufferSearchIndex.Value;
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (CommandBuffer.Count == 0)
+                return;
+
+            if (CommandBufferSearchIndex == null)
+            {
+                CommandBufferSearchIndex = CommandBuffer.Last;
+            }
+            else if (CommandInputField.text.Trim() == "")
+            {
+            }
+            else if (CommandBufferSearchIndex.Next != null)
+            {
+                CommandBufferSearchIndex = CommandBufferSearchIndex.Next;
+            }
+
+            CommandInputField.text = CommandBufferSearchIndex.Value;
+        }
+    }
+}
---
+++ GUIConsole/Assets/Scripts/Console/IGConsole.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using GUIConsole.Console;
+
+public class IGConsole : MonoBehaviour
+{
+    public static IGConsole Instance { get; set; }
+
+    public GameObject MainConsoleContent;
+    public InGameConsole Main;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Init()
+    {
+        MainConsoleContent = GameObject.Find("MainConsoleContent");
+
+        Main = MainConsoleContent.GetComponent<InGameConsole>();
+    }
+
+    public void println(string msg)
+    {
+        string[] arr = msg.Split('\n');
+        foreach (var item in arr)
+        {
+            Main.println(item);
+        }
+    }
+
+    public void println(string msg, bool DisplayTime)
+    {
+        Main.println(msg, DisplayTime);
+    }
+
+    public void printWarnln(string msg)
+    {
+        Main.printWarnln(msg);
+    }
+}
